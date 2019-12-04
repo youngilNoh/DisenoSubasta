f64c3fb5b0fed102ed0b5fee3aea426faaa21248
youngilNoh <yesimnoh@gmail.com>
2019-12-04 23:56:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/disenosubasta/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.disenosubasta;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+
+        } finally {
+            Intent intent=new Intent(this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
